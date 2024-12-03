e35f98e834091fce3217f4204dd149adf3fc0c30
Darken-027 <torut1638@gmail.com>
2024-12-02 20:15:31 -0500

    primer commit

---
+++ src/main/java/problemasLogica/Mensaje_Bienvenida.java
@@ -0,0 +1,28 @@
+/*
+    Programa que imprime un mensaje de bienvenida
+    Autor: Juan Dadiv Plaza
+    Fecha: 30 Nomviembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+
+/* 
+Esta es una funcion para mostrar un mensaje en pantalla 
+Parametros:
+El mensaje que se desea mostrar
+ */
+public class Mensaje_Bienvenida {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al programa de ingenieria de Sistemas de Computacion");
+    }
+}
---
+++ src/main/java/problemasLogica/Mensaje_Personalizado.java
@@ -0,0 +1,65 @@
+/*
+    Programa que imprime un mensaje de bienvenida personalizado
+    Autor: Juan Dadiv Plaza
+    Fecha: 1 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema: 
+Juan, requiere un programa  que permita mostrar un mensaje de bienvenida para un cliente que entra asu 
+tienda de videojuegos. Para esto su asistente carlos le solicita el nombre al ingresar a la tienda, de tal
+manera que se observe en la pantalla el mensaje a: "Bienvenido Jorge a la tienda", suponiendo que el cliente
+se llame "Jorge".
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+
+    /* Clase principal */
+public class Mensaje_Personalizado {
+    
+    /* funcion principal */
+    public static void main(String args[]){
+        Scanner entrada = new Scanner(System.in);
+        String nombre = "";
+        
+        System.out.println("Ingrese el nombre del cliente");
+        nombre = entrada.nextLine();
+        
+        System.out.println("Bienvenid@: " + nombre + " a la tienda");
+    }
+}
+
+
+
+/*
+Abstraccion:
+- Que se solicita finalmente?(problema)
+   Mostrar un mensaje de bienvenida para un cliente
+
+- Que informacion es relevante dado el problema anterior?
+    El nombre del cliente
+    Mensaje: "Bienvenido Jorge a la tienda" suponiendo que el cliente se llame "Jorge"
+    
+
+Desconposion:
+-Que acciones se requieren para resolver el problema
+    Ingresar untexto(nombre del cliente)
+    Generar mensaje de bienvenida
+    Mostrar el mensaje
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+    La funcion mostrar_mensaje
+*/
