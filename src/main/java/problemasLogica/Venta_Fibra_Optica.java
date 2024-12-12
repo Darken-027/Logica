a3e57bc78300aebecc02411bb766464376fba878
Darken-027 <torut1638@gmail.com>
2024-12-11 22:29:11 -0500

    ejercicio de calculo de precios de fibra optica

---
+++ src/main/java/problemasLogica/Venta_Fibra_Optica.java
@@ -0,0 +1,52 @@
+
+/*
+    Programa que 
+    Autor: Juan david Plaza
+    Fecha: 11 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+
+/*
+Problema:
+La empresa "Comunicaciones LTDA]", necesita un programa que le permista determinar la cantidad de rollos
+de fibra optica que necesita segun un pedido dado en metros.
+
+Ademas necesita conocer la cantidad adicional de metros de fibra que requiere para completar el pedido
+(Si aplica).
+
+Los rollos de fribra optica que la empresa vende son de 50 metros.
+
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class Venta_Fibra_Optica {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
