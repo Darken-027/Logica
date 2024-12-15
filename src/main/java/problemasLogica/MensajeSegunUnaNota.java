b201fdd0b149b98cc57ee3caaa7783f2dfe5db5f
Darken-027 <torut1638@gmail.com>
2024-12-15 06:59:22 -0500

    Problema que muestra un mensaje segun la nota definitiva de un estudiante

---
+++ src/main/java/problemasLogica/MensajeSegunUnaNota.java
@@ -0,0 +1,52 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 14 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+La profesora de matematicas necesita un mensaje segun la nota definitica de un estudiante.
+
+La nota definitiva esta entre  0.0 y 5.0, el mensaje se obtiene de la tabla.
+
+Mensaje segun la nota
+Nota definitiva -- Mensaje
+    < 3.0           Insuficiente
+    <= 3.5          Aceptable
+    <= 4.0          Sobresaliente
+    <= 5.0          Exelente
+*/
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class MensajeSegunUnaNota {
+    
+}
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
