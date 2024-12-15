67c3148233db13ebd01ff6e283084ab1a49d1243
Darken-027 <torut1638@gmail.com>
2024-12-15 06:36:34 -0500

    Problema que determina el ciclo de vida de una persona segun su edad

---
+++ src/main/java/problemasLogica/EtapaCicloDeVida.java
@@ -0,0 +1,48 @@
+/*
+    Programa que determina el ciclo de vida de una persona, segun su edad 
+    Autor: Juan david Plaza
+    Fecha: 15 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+
+/*
+Problema:
+La Organizaion mundial de la salud OMS, necesita un programa que le ayude a determinar en que ciclo de vida
+se encuentra una persona segun su edad
+
+El ciclo de vida de una persona se determina con la siguiente tabla
+ VOLVER A VER EL VIDEO EN YOUTUBE PARA VER LA TABLA
+VIDEO #18 DE LOGICA DE PROGRAMACION..
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class EtapaCicloDeVida {
+    
+}
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
