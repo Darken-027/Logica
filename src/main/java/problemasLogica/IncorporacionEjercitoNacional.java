04f762c82bda1e37160cb69e3b78358a10045863
Darken-027 <torut1638@gmail.com>
2024-12-15 21:50:13 -0500

    programa que determina si una persona es apta para ser soldado

---
+++ src/main/java/problemasLogica/IncorporacionEjercitoNacional.java
@@ -0,0 +1,55 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 15 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+La oficina de incorporacion del ejercito nacional necesita un programa que le permista saber si un 
+aspirante a soldado es apto o no para ser soldado.
+
+Para que una persona sea apta, debe los siguientes requisitos
+
+-Si es mujer:
+    -su estatura debe ser superior a 1.60mts
+    -su edad debe estar entre 20 y 25 años
+-Si es hombre:
+    -su estatura debe ser superior a 1.65mts
+    -su edad debe estar entre los 18 y 24 años
+
+tanto hombre como mujer deben ser solteros
+
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class IncorporacionEjercitoNacional {
+    
+}
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
