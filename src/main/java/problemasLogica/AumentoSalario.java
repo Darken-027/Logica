eb476a180f013a2fa475ead7cb98e8b8ac36f887
Darken-027 <torut1638@gmail.com>
2024-12-15 21:38:51 -0500

    progra que calcula aumento salarial

---
+++ src/main/java/problemasLogica/AumentoSalario.java
@@ -0,0 +1,57 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 15 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+La tienda PILATOS SA decide hacer un aumento de salario a sus empleados dependiendo de la experiencia
+laboral de cada uno de ellos, para tal efecto tendra los años de experiencia y la siguiente tabla
+
+Aumento del salario
+Años de experiencia     %
+    2-5                 10%
+    8-12                10%
+    21-25               15%
+
+Por talmotivo, la empresa lo contrata para crear un programa que permita determinar el porcentaje de 
+aumento y calcular, el salario final del empleado
+
+si el empleado no esta dentro de estos intervalos, aun no resivira aumento
+
+*/
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class AumentoSalario {
+    
+}
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
