824c057c2fdfa6fe190c8bb2383cf6c51ad6c22c
Darken-027 <torut1638@gmail.com>
2024-12-16 22:22:57 -0500

    clase que calcula la velocidad de vehiculos sospechosos

---
+++ src/main/java/problemasLogica/PersecucionPolicial.java
@@ -0,0 +1,58 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 16 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+Edison es un patrullero de la policia y entre sus actividades esta la de alcanzar y detener vehiculos
+sospechosos
+
+El sabe a que velocidad promedio viajan los vehiculos (Km/h)(por los radares instalados en las vias)
+asi como el tiempo trascurrido la infraccion, Ademas conoce la velocidad maxima de una de las motos
+que tiene a disposicion(Km/h).
+
+Edison necesita un programa que le informe el tiempo de intercepcion (Si es positivo) asi como el tiempo)
+asi es negativo, informar que no hay forma de alcanzar al infractor, en otro caso informar uno de los 
+siguientes cursos de accion:
+
+    -Salir a¿ la persecucion(Tiempo menor o iguial a 45 minutos)
+    -Llamar al siguiente puesto de control y salir en persecucion(De 45 minutos a una hora y media)
+    -Unicamente llamar al siguiente puesto de control(Desde una hora y media hasta dos horas)
+    -ignorar el caso(mas de dos horas)
+
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class PersecucionPolicial {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
