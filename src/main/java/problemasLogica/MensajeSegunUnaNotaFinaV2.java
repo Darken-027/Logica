42dd6950380a2dba0819bd684613bafd3359d1c2
Darken-027 <torut1638@gmail.com>
2024-12-17 06:28:11 -0500

    clase que valida las notas reales de un colegio

---
+++ src/main/java/problemasLogica/MensajeSegunUnaNotaFinaV2.java
@@ -0,0 +1,46 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 17 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+La Profesora quedo muy contenta con la solucion que le ayuada a evitar el error de digitacion, pero quiere
+que el programa le ayude a controlar que ingrese notas validas, es decir nostas entre la nota minima del 
+colegio(0.0) y la notamaxima(5.0).
+
+*/
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class MensajeSegunUnaNotaFinaV2 {
+    
+}
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
