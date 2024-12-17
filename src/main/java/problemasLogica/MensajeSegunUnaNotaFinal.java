96e5ce1421e89ab584d7f0a4930bf5b909dc16a4
Darken-027 <torut1638@gmail.com>
2024-12-16 22:39:26 -0500

    clase que calcula la nota final y dice si un estudiante aprovo o reprovo

---
+++ src/main/java/problemasLogica/MensajeSegunUnaNotaFinal.java
@@ -0,0 +1,50 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 16 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+La profesora de matematicas del colegio laureles mecesita una aplicacion que le permita ingresar una nota
+final y obtener un mensaje que indique si el estudiante APRUEVA o REPRUEVA el curso.
+
+Se sabe que un estudiante aprueba el curso si obtiene al menos una nota final de 3.0
+
+Ademas, ella no quiere que la aplicacion deje de funcionar cada vez que por error se ingrese algo que no 
+sea un numero(Valide la entrada)
+
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class MensajeSegunUnaNotaFinal {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
