93131c7d99a520f962c350572c0b4f1c9797ba6a
Darken-027 <torut1638@gmail.com>
2024-12-17 06:51:58 -0500

    programa que genera tablas de multiplicar automaticamente

---
+++ src/main/java/problemasLogica/LasTablasDeMultiplicar.java
@@ -0,0 +1,48 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 17 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+Sofia quedo muy contenta con el programa que genera la tabla de multiplicar indicada, pero ahora ella quiere
+qie el programa genere automaticamente todas las tablas de multipliar entre dos valores que se encuentran 
+entre cero y veinte (Ambos valores incluidos).
+
+El programa debe asegurarse que el segundo valor sea mayor o igual al primer valor.
+
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class LasTablasDeMultiplicar {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
