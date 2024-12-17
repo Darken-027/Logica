a51e9757fb2a7de62620059f6b8f1e9c3c4f9ff1
Darken-027 <torut1638@gmail.com>
2024-12-17 06:36:35 -0500

    clase que crea un listado de asistentes para que puedan firmar

---
+++ src/main/java/problemasLogica/ListadoDeAsistentes.java
@@ -0,0 +1,53 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 17 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+La directora de jardin infantil "mis pequeñitos" desea una aplicacion que le permita crar un listado para
+que los asistentes a una reunion puedan firmar.
+
+El programa debe permitir indicar el valor para el primero de la lista como para el ultimo de la lista.
+El listado debe tener un lugar para que cada persona firme(Uno por renglon).
+
+Ademas la directora quiere.
+    -evitar que el programa se cansele si se ingresa un valor invalido
+    -Evitar comenzar con un valor negativo
+    -evitar que el usuario ingrese como ultimo valor, un valor inferior o igual al valor asignado
+      al primero de la lista.
+
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class ListadoDeAsistentes {
+    
+}
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
