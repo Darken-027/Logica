5d9e063ef7b026a69575a6533d28c92ffcf76d4c
Darken-027 <torut1638@gmail.com>
2024-12-17 06:57:54 -0500

    programa que cuenta valores en frecuenta de dialectos

---
+++ src/main/java/problemasLogica/ContarValores.java
@@ -0,0 +1,48 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 17 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+El colegio Gimnasio Anglosajon esta realizando una investigacion sobre la frecuencia de las vocales en 
+diferentes dialectos de un idioma determinado.
+
+para ello necesita un programa que cuente el numero de veces  que aparecen las vocales(Frecuencia) en una
+frace (Respuesta dada por un encuestado)
+
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class ContarValores {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
