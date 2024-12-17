37ef761f79befbfd36cabf564756b3b8e8cff321
Darken-027 <torut1638@gmail.com>
2024-12-17 06:44:43 -0500

    clase qu egenerea una tabla de multiplicar

---
+++ src/main/java/problemasLogica/TablaDeMultiplicar.java
@@ -0,0 +1,47 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 17 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+Sofia una profesora de matematicas quiere un programa que le genere una tabla de multiplicar(Desde el valor
+de cero hasta diez).
+
+El numero de la tabla a generar es indicado por el usuario y esta registrado hasta el valor veinte.
+
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class TablaDeMultiplicar {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
