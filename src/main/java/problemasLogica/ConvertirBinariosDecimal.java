fcd3bf4e1c653ad022f6b058bbebbcab88bcf698
Darken-027 <torut1638@gmail.com>
2024-12-18 06:55:50 -0500

    clase convierte Binarios a numeros decimales

---
+++ src/main/java/problemasLogica/ConvertirBinariosDecimal.java
@@ -0,0 +1,55 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 18 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+Jorge el profesor de introduccion a la ingenieria necesita un programa para que los estudiantes practiquen 
+la conversacion entre bases de un numeroentero.
+
+El numero binario se debe debe ingresar como una cadena de 8 caracteres
+(numero de 8bits) y el programa lo debe garantizar
+
+por ejemplo
+
+numeros enteros base 2(Binarios)                Numero enteros base 10(Desimal)
+    00000000                                                0
+    00000010                                                2
+    00000101                                                5    
+    01010101                                                85                
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class ConvertirBinariosDecimal {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
