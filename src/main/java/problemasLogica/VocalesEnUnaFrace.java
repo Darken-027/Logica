b71168511d368899cfcfefa8140e0a9a49d2bd57
Darken-027 <torut1638@gmail.com>
2024-12-17 22:20:45 -0500

    vacales en una frace

--- src/main/java/problemasLogica/VocalesEnUnaFrace.java
+++ src/main/java/problemasLogica/VocalesEnUnaFrace.java
@@ -1,4 +1,20 @@
 /*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 17 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+El colegio Anglosajon quedo muy contento con el programa que cuenta el numero de veces que aparecen 
+las vocales
+
+pero, ahora el colegio quiere identificarce
+
+*/
+
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
@@ -11,3 +27,20 @@ package problemasLogica;
 public class VocalesEnUnaFrace {
     
 }
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
