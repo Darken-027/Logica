ae5314999e97e34552e7e4f48ae863312860e046
Darken-027 <torut1638@gmail.com>
2024-12-18 07:17:58 -0500

    Clase que convierte de numero decimal a numeros Binarios

---
+++ src/main/java/problemasLogica/ConvertirDecimalBinarios.java
@@ -0,0 +1,59 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 18 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+Ahora el profesor jorge necesita el programa que realiza el proceso inverso, es decir convertir  un numero 
+entero base 10, a su equivalente en base 2.
+
+    *El numero decimal se debe ingresar como un numero entero 0 y 255
+    *El numro binario se debe entrgar como una cade de longitud de 8 caracteres,
+        esdecir un numero maximo 8bits(2 ala 8 equivale a 255)
+    *El programa debe garantizar la longitud del numero de salida, de ser necesario se debe completar con
+    ceros.
+
+
+por ejemplo
+
+numeros enteros base 2(Binarios)                Numero enteros base 10(Desimal)
+    0                                                   00000000                                               
+    2                                                   00000010                                               
+    5                                                   00000101                                                   
+    85                                                  01010101                                                              
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class ConvertirDecimalBinarios {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
