4b1b3f2ae318ed8b634362d0880c532647a32321
Darken-027 <torut1638@gmail.com>
2024-12-19 07:02:35 -0500

    clase que convierte un texto a codigo ASCII

---
+++ src/main/java/problemasLogica/CodigosASCII.java
@@ -0,0 +1,43 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 19 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+                                                 01010101                                                              
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class CodigosASCII {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
