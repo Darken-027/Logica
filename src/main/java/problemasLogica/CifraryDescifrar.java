46a83224de7d79b065b7aa9a0fde8e05ef2a0dcc
Darken-027 <torut1638@gmail.com>
2024-12-19 07:21:05 -0500

    descifrar y cifrar un texto

---
+++ src/main/java/problemasLogica/CifraryDescifrar.java
@@ -0,0 +1,56 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 19 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+La empresa de seguridad "security.gov" quedo muy contenta con el programa que convierte los caracteres
+de una frace en sus equivalentes en codigo ASCII, pero ahora quiere usar este programa para hacer sifrado y
+descifrado Cesar.
+
+El cifrado cesar es uno de los cifrados mas conocidos y antiguos de la humanidad y consiste en tomarcada
+caracter de una frase y desplazarlo en el alfabecto un determinado numero de posiciones, por ejemplo, "A" 
+si se desplaza 2 posiciones se convierte en "C", asi para todas las letras del mensaje
+
+Para descifrar un mensaje se hace el proceso inverso
+
+Por ejemplo:
+El mensaje "Hola" usando desplazamiento de una posicion se obtiene el mensaje "lpmb" y se descifra(desplaza
+miento menos una posicion) para obtener nuevamente hola
+
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class CifraryDescifrar {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
