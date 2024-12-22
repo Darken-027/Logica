760338aae533eef2a5c5beea22fdf713bd096673
Darken-027 <torut1638@gmail.com>
2024-12-21 22:41:54 -0500

    sucesion fibonacci

---
+++ src/main/java/problemasLogica/SucesionFibonacci.java
@@ -0,0 +1,56 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 21 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+Orlando el profesor de matematicas de la universidadnecesita un programa para determinar cuantos numeros
+la sucesion infinita de fibonacci hay entre dos valores (mayores que cero) indicados por el, Ambos valore
+incluidos, el segundo valor debe ser mayor que el primero.
+ 
+la sucesion de fibonacci(1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,1597)
+
+La sucesion inicia con los valores de 1 y 1 el siguiente termino se generna sumando los numeros anteriores,
+y asi hasta el infinito
+
+Si por ejemplo, Orlando desea saber cuantos terminos de la sucesion de fibonacci hay entre los valores 
+4 y 15 la respuesta seria 3, el profesor no necesita que el programa indique cuales son esos terminos,
+pero para este caso son (5,8,13)
+
+
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class SucesionFibonacci {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
