a0a759a39b96bd5df522f3894348b01ad64beb29
Darken-027 <torut1638@gmail.com>
2024-12-22 17:51:18 -0500

    Clase que capitaliza palabras

---
+++ src/main/java/problemasLogica/CapitalizandoPalabras.java
@@ -0,0 +1,52 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 22 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+La profesora de lengua de un colegio necesita un programa que le ayude asus estudiantes a 
+practicar con la capitalizacion  de las palabras en una frese.
+
+Una palabra esta capitalizada si todas sus letras estan en minusculas, salvo la primera,
+por ejemplo, cuando se escribe un nombre propio "Carlos Alberto"
+
+El programa debe permitir al estudiantes ingresar una oracion y luego mostrar dos salidas 
+diferentes.
+    -la oracion completamente en minusculas
+    -la oracion con todas sus palabras capitalizadas
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class CapitalizandoPalabras {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
