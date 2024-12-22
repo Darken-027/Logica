e5eb244f34b8f25c5c479a132ebcbc0f2de94fc5
Darken-027 <torut1638@gmail.com>
2024-12-22 18:02:50 -0500

    TKINTER

---
+++ src/main/java/problemasLogica/TkInter.java
@@ -0,0 +1,48 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 22 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+Juan manuel, uno de nuestros estudiantes desea que los programas que ha desarrollado en este curso de
+Logica de programacion le funcione con una interfaz grafica (GUI Graphical User Interface) haciendo el menor
+numero de cambios posibles
+
+Ademas quiere volver a la forma tradicional de entrada y salida de datos
+(CLI-Command-Line Interface) cuando lo desee
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class TkInter {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
