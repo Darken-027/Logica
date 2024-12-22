67ea734987e284c4936851ef95c5c3e07c9fa346
Darken-027 <torut1638@gmail.com>
2024-12-22 18:52:32 -0500

    venta de troncos de madera

---
+++ src/main/java/problemasLogica/VentaDeTroncos.java
@@ -0,0 +1,51 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 22 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+Diseños S.A  es una empresa que vende madera y necesita programa que le permita ingresar la cantidad de
+troncos que tiene disponible en la bodega, y luego ingresar el grosor y el largo de cada tronco en
+milimetros(mm)
+
+Posteriormente el programa debe identificar los troncos validos para la venta
+(Grosor comprendido entre 10 a 14 cm, ambos valores incluidos) y mostrar: tanto el largo y el grosor de cada
+uno de los troncos validos, Asi como la cantidad lista para la venta
+
+tanto el grosor como el largo no puden tener decimales.
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class VentaDeTroncos {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
