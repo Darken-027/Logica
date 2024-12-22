c00e3d0a3613a2857bef57630a6b87e96da5134e
Darken-027 <torut1638@gmail.com>
2024-12-22 18:40:44 -0500

    Estudiantes mayores y menores de edad

---
+++ src/main/java/problemasLogica/EstudiantesMayoresMenoresEdad.java
@@ -0,0 +1,51 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 22 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+Julian, profesor de la clase de estadistica, necesita identificar los nombres y la cantidad de estudiantes
+que son mayores de edad, asi como lo que son menores de edad.
+
+El programa debe inpedir ingresar cantidades y edades negativas,
+asi como edades superiores a  120 años.
+
+
+Hay que considerar que en colombia la mayoria de edad se cumple a los 18 años.
+*/
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class EstudiantesMayoresMenoresEdad {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
