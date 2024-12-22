370f8db9e0e3ca4588b6be852ce4822ecd3351bb
Darken-027 <torut1638@gmail.com>
2024-12-21 23:02:29 -0500

    figura con caracteres

---
+++ src/main/java/problemasLogica/FiguraConCaracteres.java
@@ -0,0 +1,55 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 21 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+Ximena una estudiante del doctorado en diseño grafico, necesita un programa que le ayude a crear un 
+dibujo dependiendo de un valor n(Mayor que cero) que ella defina
+
+ejemplo
+
+n = 3
+
+********
+**..**
+*...*
+********
+
+
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class FiguraConCaracteres {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
