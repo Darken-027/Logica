a03b722b4ef615f836683443151b96a0e70b5541
Darken-027 <torut1638@gmail.com>
2024-12-22 19:46:57 -0500

    orden una lista de estudiantes de un curso

---
+++ src/main/java/problemasLogica/OrdenarUnaLista.java
@@ -0,0 +1,44 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 22 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+El profesor carlos del programa I, necesita de un programa que le permita mostrar el listado de  los estudiantes
+de un curso, tanto de forma descendente, como el orden en el que fueron ingresados.
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class OrdenarUnaLista {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
