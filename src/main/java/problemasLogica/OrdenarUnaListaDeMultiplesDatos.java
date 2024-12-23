1c207c2eec8fd231707383870445466fe1039f8d
Darken-027 <torut1638@gmail.com>
2024-12-22 20:10:48 -0500

    odenar listado de multiples datos

---
+++ src/main/java/problemasLogica/OrdenarUnaListaDeMultiplesDatos.java
@@ -0,0 +1,48 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 22 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+El rector de la institucion educativa mis pequeños genios, necesita una aplicacion que le solicite el nombre
+de los estudiantes de la institucion y su nacionalidad, posteriormente el programa debe mostrar el listado 
+de los estudiantes ordenados ascendetemente por nombre(Incluyendo la nacionalidad) y luego el listado 
+ordenado por las nacionalidades (incluyendo el nombre del estudiante)
+
+*/
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class OrdenarUnaListaDeMultiplesDatos {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
