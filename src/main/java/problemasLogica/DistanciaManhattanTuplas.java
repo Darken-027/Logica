4fac09026be5d9b0f96e9e93f46b41ba749e1472
Darken-027 <torut1638@gmail.com>
2024-12-22 20:04:31 -0500

    distancia manhattan

---
+++ src/main/java/problemasLogica/DistanciaManhattanTuplas.java
@@ -0,0 +1,49 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 22 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+Milena una estudiante del colegio monterry, escucho del profesor de matematicas que hay otra forma de 
+calcular la distancia entre dos puntos ademas de la distancia euclidiana, la denominada distancia
+Manhattan, asi que ella quiere una aplicacion que le ayude a calcular la distancia manhattan entre dos
+puntos en un plano bidimensional
+
+*/
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class DistanciaManhattanTuplas {
+    
+}
+
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
