bc2d139c02f4a87382ad9ddc9f191c342231a588
Darken-027 <torut1638@gmail.com>
2024-12-23 23:12:27 -0500

    asistentes a fiestas

---
+++ src/main/java/problemasLogica/AsistentesFiestas.java
@@ -0,0 +1,44 @@
+/*
+    Programa que muestra un mensaje segun la nota definitiva de un estudiante
+    Autor: Juan david Plaza
+    Fecha: 22 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+
+
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class AsistentesFiestas {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
