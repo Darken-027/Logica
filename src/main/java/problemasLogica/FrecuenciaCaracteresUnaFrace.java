3cb379d85a62d1e2862d7c8bfb3008830aba979f
Darken-027 <torut1638@gmail.com>
2024-12-24 21:41:29 -0500

    frecuencia de caracteres en una frace

---
+++ src/main/java/problemasLogica/FrecuenciaCaracteresUnaFrace.java
@@ -0,0 +1,48 @@
+/*
+    Programa 
+    Autor: Juan david Plaza
+    Fecha: 23 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+Diego el profesor de literactura de la universidad, lo contracta para crear una aplicacion que le permita
+determinar la cantidad de veces que aparece cada caracter(Letra, simbolo, numero) en una frace.
+
+La salida la quiere ordenada de mayor a menor segun la frecuencia, pero limitada a loscaracteres con
+ una frecuencia mayor a uno.
+
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class FrecuenciaCaracteresUnaFrace {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
