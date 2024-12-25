2b33ac41e329cac3fd4398693dfff54ca9c005ef
Darken-027 <torut1638@gmail.com>
2024-12-24 22:31:43 -0500

    ultima clase con el problema

---
+++ src/main/java/problemasLogica/NumeroDeReinasRecursividad.java
@@ -0,0 +1,51 @@
+/*
+    Programa 
+    Autor: Juan david Plaza
+    Fecha: 24 Diciembre 2024
+    Licencia: GNU GPL v3
+*/
+
+/*
+Problema:
+Mauricio, Uno de sus compañeros del programa de ingenieria de sistemas y computacion le contacta para que
+le explique y le ayude a resolver cinco problemas clasicos de la recursividad
+
+    -Calcular el factorial de un numero entero
+    -Determinar el valor del n-esimo termino de la siccion de fibonacci
+    -Decidir si un numero entero positivo es par o impar
+    -Encontrar las soluciones del problema de las n-rinas
+    -Encontrar todas las soluciones del laberinto
+
+*/
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problemasLogica;
+
+/**
+ *
+ * @author Juan Plaza
+ */
+public class NumeroDeReinasRecursividad {
+    
+}
+
+
+/*
+Abstraccion
+-Que se solicita finalmente?(Problema)
+    
+
+-Que informacion es relevante dado el problema anterior ?
+    
+
+Descomposicion
+-Que acciones se requieren para resolver el problema
+    
+
+Reconocimiento de patrones
+-Que puedo reutilizar de la solucion de otros problemas ?
+     
+*/
